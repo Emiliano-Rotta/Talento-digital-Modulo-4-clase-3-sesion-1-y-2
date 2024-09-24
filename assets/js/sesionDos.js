0b7f6656cf0b6a34164a24ff0d84b188ef3964ef
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-23 21:21:09 -0300

    ejercicios de generadores

--- assets/js/sesionDos.js
+++ assets/js/sesionDos.js
@@ -24,42 +24,106 @@
 
 // // Se espera: "El área del rectángulo es: 50"
 
-class Rectangulo {
-    constructor(alto, ancho) {
-        this.alto = alto;
-        this.ancho = ancho;
-    }
-    calcularArea(){
-        return this.alto * this.ancho  
-        //respuesta de David
-        //var area = this.alto * this.ancho
-        //return `El áreas del rectangulo es: ${area}`
-    }
+// class Rectangulo {
+//     constructor(alto, ancho) {
+//         this.alto = alto;
+//         this.ancho = ancho;
+//     }
+//     calcularArea(){
+//         return this.alto * this.ancho  
+//         //respuesta de David
+//         //var area = this.alto * this.ancho
+//         //return `El áreas del rectangulo es: ${area}`
+//     }
+// }
+// const miRectangulo = new Rectangulo(10,5)
+
+// console.log("El área del rectángulo es: " + miRectangulo.calcularArea() )
+
+// //------------------------------------
+
+// // Ejercicio 3: Clase "Persona" con método que salude
+// // Consigna: Crea una clase llamada Persona con las propiedades nombre y edad. Incluye un método llamado saludar que retorne un mensaje como "Hola, mi nombre es [nombre] y tengo [edad] años". Crea al menos dos instancias de la clase y haz que cada una salude mostrando el resultado en la consola.
+
+// // // Se espera: Hola, mi nombre es Emiliano y tengo 31 años.
+// // // Se espera: Hola, mi nombre es Carlos y tengo 30 años.
+
+// class Persona {
+//     constructor(nombre, edad){
+//         this.nombre = nombre
+//         this.edad = edad
+//     }
+//     saludar(){
+//         return `Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años`;
+//     }
+// }
+
+// const personaUno = new Persona("Emiliano", 31)
+// const personaDos = new Persona("Carlos", 30)
+
+// console.log(personaUno.saludar())
+// console.log(personaDos.saludar())
+
+//--------------------- extend -------------------------
+
+
+// class Vehiculo {
+//     constructor(tipoVehiculo){
+//         this.tipoVehiculo = tipoVehiculo;
+//     }
+// }
+// class Auto extends Vehiculo{
+//     constructor(marca, modelo, anio, tipoVehiculo){
+//         super(tipoVehiculo)
+//         this.marca = marca;
+//         this.modelo = modelo;
+//         this.anio = anio;
+//     }
+//     detallesAuto() {
+//         return `Tipo: ${this.tipoVehiculo}, Marca: ${this.marca}, Modelo: ${this.modelo}, Año:${this.anio}.`
+//     }
+// }
+
+// const miAuto = new Auto("renault", "kangoo", 2022, "Auto")
+// console.log(miAuto.detallesAuto())
+
+//-----------------------------------------------------------------
+//              Módulos y generadores
+
+
+let frutas = ['manzana', 'naranja', 'pera', 'frutilla', 'kiwi']
+// let i = 3
+// console.log(frutas[i])
+
+
+// configuramos el generador 
+function* generador(){
+    let i = 0
+    yield frutas[i];  //pausamos la funcion..
+    i++
+    yield frutas[i];
+    i++
+    yield frutas[i];
+    i++
+    yield frutas[i];
+    i++
+    yield frutas[i];
+    i++
+    return "terminado.........."
 }
-const miRectangulo = new Rectangulo(10,5)
 
-console.log("El área del rectángulo es: " + miRectangulo.calcularArea() )
+let genUno = generador()
+console.log(frutas)
 
-//------------------------------------
+console.log(genUno.next().value)
+console.log(genUno.next().value)
+console.log(genUno.next().value)
+console.log(genUno.next().value)
+console.log(genUno.next().value)
+console.log(genUno.next().value)
 
-// Ejercicio 3: Clase "Persona" con método que salude
-// Consigna: Crea una clase llamada Persona con las propiedades nombre y edad. Incluye un método llamado saludar que retorne un mensaje como "Hola, mi nombre es [nombre] y tengo [edad] años". Crea al menos dos instancias de la clase y haz que cada una salude mostrando el resultado en la consola.
 
-// // Se espera: Hola, mi nombre es Emiliano y tengo 31 años.
-// // Se espera: Hola, mi nombre es Carlos y tengo 30 años.
+//Ejercicio 1
+ // Consigna: Tienes un array de colores: ['rojo', 'azul', 'verde', 'amarillo', 'morado']. Crea una función generadora llamada generadorColoresInverso que devuelva los colores en orden inverso (de morado a rojo) utilizando yield. Después de haber devuelto todos los colores, la función debe terminar con el mensaje "Se acabaron los colores". Usa i-- para recorrer el array de atrás hacia adelante.
 
-class Persona {
-    constructor(nombre, edad){
-        this.nombre = nombre
-        this.edad = edad
-    }
-    saludar(){
-        return `Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años`;
-    }
-}
-
-const personaUno = new Persona("Emiliano", 31)
-const personaDos = new Persona("Carlos", 30)
-
-console.log(personaUno.saludar())
-console.log(personaDos.saludar())
+// let colores = ['rojo', 'azul', 'verde', 'amarillo', 'morado'];
